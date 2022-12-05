486e9740818d020cf9d83ceca27bf3c074d8a18f
outragep <53834450+outragep@users.noreply.github.com>
2022-12-05 22:36:32 +0300

    Add files via upload

---
+++ age.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+  
+int main()                 
+{                                   
+    int age;
+        age =28;
+        std::cout<<"Age = "<<age;
+        cin.get();
+        return 0;
+}
